b2ce145cb230c6a4e08c9265235f52f5d3cb81dc
Pablo Emmanuel De Leo <pabloemmanueldeleo@gmail.com>
2020-06-08 16:11:50 -0300

    limpieza

--- PRIMERA_CLASE_PYTHON/LIT/PrimerScript.py
+++ PRIMERA_CLASE_PYTHON/LIT/PrimerScript.py
@@ -1,14 +1,15 @@
-#importamos el modulo
 import pymel.core as pm
-#creamos las variables a usar.
+import math
 lit = []
 LG = pm.group(em=True,name='LIT_GRP')
 rotacion=0
-cantidadDeLuces=22
-radioDeTodasLasLuces=20
-scalaPersonal=3
 
-for n in range(1,cantidadDeLuces):
+radio=20
+luces=20
+
+scala=radio*math.pi/luces
+
+for n in range(1,luces+1):
     #Generamos nombre de la luz
     newName='LUZ_{0}_AIAL'.format(str(n).zfill(3))
     #Creamos un grupo para usarlo temporalmente
@@ -22,16 +23,15 @@ for n in range(1,cantidadDeLuces):
     #Emparentamos el padre de la luz en el grupo temporal
     pm.parent(pl,grupoTemporal)
     #movemos la el transform de la luz en x positivo 10
-    pl.setAttr('translateX',radioDeTodasLasLuces)
+    pl.setAttr('translateX',radio)
     #rotamos el transform de la luz en y 90 grados
     pl.setAttr('rotateY',90)
-    #Escalamos las luces para que queden parejas
-    calculoScala=scalaPersonal
-    pl.setAttr('scaleX',calculoScala)
-    pl.setAttr('scaleY',calculoScala)
-    pl.setAttr('scaleZ',calculoScala)
     #rotamos el grupo temporal su transform en Y por cada vuelta le sumamos la rotacion
     grupoTemporal.setAttr('rotateY',rotacion)
+    #Escalamos las luces para que queden parejas
+    pl.setAttr('scaleX',scala)
+    pl.setAttr('scaleY',scala)
+    pl.setAttr('scaleZ',scala)
     #Emparento el transform de la luz al grupo maestro principal
     pm.parent( pl, LG )
     #Ahora borramos el grupo temporal para limpiar
@@ -39,4 +39,4 @@ for n in range(1,cantidadDeLuces):
     #Guardamos las luces dentro de la variable lit desde su transfomr
     lit.append([l,pl])
     #Al final le sumamos a la variable rotacion 18 para seguir rotando
-    rotacion+= 18
+    rotacion+= 360/luces
